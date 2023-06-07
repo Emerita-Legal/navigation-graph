086dd9422a335779018957c39c9bd8c073a1fb9f
Hernán de la Fuente <hernandelafuentelopez@gmail.com>
2023-06-07 12:44:30 +0200

    Change message text

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -31,16 +31,17 @@ export class ChatService {
       this.conversation.getValue().addMessage(
         new Message(
           ++this.messageId,
-          `Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-        Labore a voluptates corporis illum quae, quasi esse dicta vitae maxime aspernatur? 
-        Earum atque reiciendis odit molestiae culpa quisquam fuga repellat`,
+          `La probabilidad de éxito con los factores que indicas es del 62%. Para ello, he considerado
+          la tasa de éxito en casos de despido disciplinario en los que se defiende al trabajador en los
+          Juzgados de lo Social de A Coruña de los últimos cinco años y la tasa de éxito promedio del
+          abogado contrario con los mismos factores.
+          Aquí puedes ver la representación gráfica de las tasas de éxito.`,
           new Date(),
           'received',
           [
             new SubMessage(TextComponent, {
-              text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-              Labore a voluptates corporis illum quae, quasi esse dicta vitae maxime aspernatur? 
-              Earum atque reiciendis odit molestiae culpa quisquam fuga repellat`,
+              text: `Juzgado de lo Social de A Coruña - Evolución de la tasa de éxito en casos de despido
+              disciplinario defendiendo al trabajador.`,
             }),
             new SubMessage(LineChartComponent, [
               { year: 2018, global: 70, media: 70 },
@@ -51,13 +52,12 @@ export class ChatService {
               { year: 2023, global: 100, media: 100 },
             ]),
             new SubMessage(TextComponent, {
-              text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.`,
+              text: `Tasa de éxito de Pablo Luis Vázquez García en despidos disciplinarios.`,
             }),
             new SubMessage(CircleChartComponent, { value: 20, mediaValue: 34 }),
             new SubMessage(TextComponent, {
-              text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-              Labore a voluptates corporis illum quae, quasi esse dicta vitae maxime aspernatur? 
-              Earum atque reiciendis odit molestiae culpa quisquam fuga repellat`,
+              text: `Puedo ser más preciso calculando la probabilidad de éxito. ¿Quieres incluir
+              otros factores?`,
             }),
           ]
         )
